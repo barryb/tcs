a0fcc2e50ef2f59f63061136504bd2131fe5e556
Barry Byrne <barry.j.byrne@gmail.com>
2012-11-27 10:06:48 +0000

    use approved verbs

--- test.ps1
+++ test.ps1
@@ -7,9 +7,24 @@ $cf = [xml](Get-Content "$scriptDir\config.xml")
 
 $setup_dir = $cf.config.setup_dir.value
 $phpInstallerName = $cf.config.phpInstallerName.value
+$sql_admin_name = $cf.config.sql_admin_name.value
+$sql_admin_password = $cf.config.sql_admin_password.value
+$sql_server_name = $cf.config.sql_server_name.value
+$sql_use_windows_auth = $cf.config.sql_use_windows_auth.value
+$tc_installer_zip = $cf.config.tc_installer_zip.value
+$ask_permission = $cf.config.ask_permission.value
+$sql_admin_password = $cf.config.sql_admin_password.value
 
 Import-Module $scriptDir\TopClassTools
 
+if ( $sql_user_windows_path -eq $true ) {
 
-Install-TCiis
-Install-TCphp -setup_dir $setup_dir -phpInstallerName $phpInstallerName
+    write-host "Using Windows AUTH"
+} else {
+
+    write-host "Using SA details provided"
+}
+
+
+#Install-TCiis
+#Install-TCphp -setup_dir $setup_dir -phpInstallerName $phpInstallerName
