66cbb63795f1219ad77ba4af59678628df501a48
Barry Byrne <barry.j.byrne@gmail.com>
2012-11-27 09:40:54 +0000

    use approved verbs

--- TopClassTools.psm1
+++ TopClassTools.psm1
@@ -24,7 +24,7 @@ function Install-TCphp {
 
 
      Start-Process `
-        -file "$setup_dir\phpInstallerName" `
+        -file "$setup_dir\$phpInstallerName" `
         -arg $args `
         -passthru | Wait-Process
 
