576b35ec2fd1bd058a2b44b9508ff29b12a7072c
Barry Byrne <barry.j.byrne@gmail.com>
2012-11-27 12:11:35 +0000

    begin sql configuration

--- TopClassTools.psm1
+++ TopClassTools.psm1
@@ -17,14 +17,125 @@ function Install-TCphp {
     param (
 
         [string] $setup_dir,
-        [string] $phpInstallerName
+        [string] $installerName
         )
 
     [string] $args = "/q ADDLOCAL=iis4FastCGI /log $setup_dir\php-install.log"
 
      Start-Process `
-        -file "$setup_dir\$phpInstallerName" `
+        -file "$setup_dir\$installerName" `
         -arg $args `
         -passthru | Wait-Process
 
+}
+
+
+
+function Enable-TCclr {
+
+    param (
+            [boolean] $use_windows_auth = $true,
+            [string] $admin_name = 'sa',
+            [string] $admin_password = 'needpassword',
+            [string] $instance_name = 'MSSQLSERVER',
+            [boolean] $ask_permission = $false
+
+        )
+
+    if ( $use_windows_auth -eq $true ) {
+        $sql_creds = ""
+    } else {
+        $sql_creds = "-Username $admin_name -Password $admin_password"
+    }
+
+
+    # Add SQL Snapins if they aren't loaded
+        
+    if ( (Get-PSSnapin -Name SqlServerCmdletSnapin100 -ErrorAction SilentlyContinue) -eq $null ) {
+        Add-PSSnapin SqlServerCmdletSnapin100
+    }
+        
+    if ( (Get-PSSnapin -Name SqlServerProviderSnapin100 -ErrorAction SilentlyContinue) -eq $null ) {
+        Add-PSSnapin SqlServerProviderSnapin100
+    }
+
+    $sql_cmd = "Invoke-Sqlcmd -Database master $sql_creds -Query "
+
+    # Enable Showing advanced options
+    $command = "exec sp_configure 'show advanced options',1; reconfigure"
+    Invoke-Expression "$sql_cmd `"$command`""
+
+    # Determine if CLR is aready enabled
+    $command = "exec sp_configure 'clr enabled'"
+    $result = Invoke-Expression "$sql_cmd `"$command`""
+    $clr_enabled = $result.config_value
+
+    # Determine if Lightweight Pooling is aready enabled
+    $command = "exec sp_configure 'lightweight pooling'"
+    $result = Invoke-Expression "$sql_cmd `"$command`""
+    $lwp_enabled = $result.config_value
+
+    $action_commands = @()
+    $action_notes = @()
+
+
+    if ($clr_enabled -eq 0) {
+        
+       if ($lwp_enabled -eq 1) {
+
+            $action_notes += "Lightweight Pooling will be disabled"
+            $command = "exec sp_configure 'lightweight pooling', 0; reconfigure"
+            $action_commands += "$sql_cmd `"$command`""
+            
+            $action_notes += "SQL Server service will be restarted"
+            $svc = get-service $instance_name
+            $svc_name = $svc.name
+            $action_commands += "Restart-Service $svc_name -Force"
+            $action_commands += "`$svc.WaitForStatus('Running', (new-timespan -seconds 30))"  
+
+            # The following command is a dummy command to force a new connection after the above restart    
+            $action_commands += "$sql_cmd `"select newid()`" -ErrorAction SilentlyContinue"
+            
+       }
+       
+       $action_notes += "CLR WILL be enabled"
+       $command = "exec sp_configure 'clr enabled', 1; reconfigure"
+       $action_commands += "$sql_cmd `"$command`""
+       
+    } else {
+        Write-Host "CLR was already enabled"
+    }
+
+    Write-Host "`n`nThe following tasks will be done:`n"
+    foreach ($note in $action_notes) {
+        Write-Host $note
+    }
+
+    Write-Host "`n`nThis means the following commands will be executed:`n"
+    foreach ($action in $action_commands) {
+        Write-Host $action
+    }
+
+    $choice = ""
+
+
+    if (($ask_permission -eq $true) -and ($action_commands.length -gt 0)) {
+        while ($choice -notmatch "[y|n]") {
+            $choice = Read-Host "Are you sure you want to do this? (y/n)?"
+        }
+        if ($choice -eq "y") {
+            $do_actions = $true
+        } else {
+            $do_actions = $false
+        }
+    } else {
+        $do_actions = $true
+    }
+
+    if ( $do_actions -eq $true ) {
+        foreach ($action in $action_commands) {
+            Invoke-Expression $action
+        }
+        Write-Host "`n`nAll Done`n"
+    }
 }
--- test.ps1
+++ test.ps1
@@ -10,25 +10,19 @@ $phpInstallerName = $cf.config.phpInstallerName.value
 $sql_admin_name = $cf.config.sql_admin_name.value
 $sql_admin_password = $cf.config.sql_admin_password.value
 $sql_server_name = $cf.config.sql_server_name.value
-
 $tc_installer_zip = $cf.config.tc_installer_zip.value
-
 $sql_admin_password = $cf.config.sql_admin_password.value
-
 $ask_permission = [Boolean]::Parse($cf.config.ask_permission.value)
 $sql_use_windows_auth = [Boolean]::Parse($cf.config.sql_use_windows_auth.value)
 
 Import-Module $scriptDir\TopClassTools
 
+Install-TCiis
+Install-TCphp -setup_dir $setup_dir -installerName $phpInstallerName
 
-if ( $sql_use_windows_auth -eq $true ) {
-
-    write-host "Using Windows AUTH"
-} else {
-
-    write-host "Using SA details provided"
-}
-
-
-#Install-TCiis
-#Install-TCphp -setup_dir $setup_dir -phpInstallerName $phpInstallerName
+Enable-TCclr `
+    -use_windows_auth $sql_use_windows_auth `
+    -admin_name $sql_admin_name `
+    -admin_password $sql_admin_password `
+    -instance_name $sql_server_name `
+    -ask_permission $ask_permission
