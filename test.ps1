aa3c6e4ac63ed9fcbe81792677754776a6f387ca
Barry Byrne <barry.j.byrne@gmail.com>
2012-11-27 09:52:40 +0000

    use approved verbs

--- test.ps1
+++ test.ps1
@@ -5,8 +5,11 @@ $scriptDir = Split-Path $scriptPath
 
 $cf = [xml](Get-Content "$scriptDir\config.xml")
 
+$setup_dir = $cf.config.setup_dir.value
+$phpInstallerName = $cf.config.phpInstallerName.value
+
 Import-Module $scriptDir\TopClassTools
 
 
 Install-TCiis
-Install-TCphp -setup_dir $cf.config.setup_dir -phpInstallerName $cf.config.phpInstallerName
+Install-TCphp -setup_dir $setup_dir -phpInstallerName $phpInstallerName
